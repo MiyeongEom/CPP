185cdd739a7a02ff95f210b9a2183bff772d7e23
Eom Mi Yeong <aldjd020420@naver.com>
2023-09-12 23:55:39 +0900

    9월 12일

---
+++ lecture/01_정수.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+// 주석 ctrl+k+c / ctrl+k+u
+
+// code / data / heap / stack
+
+// data영역 내에서 .bss / .data / .rodata
+
+// 0이 아닌 초기화값이 있으면 .data영역
+// 초기값이 0이거나, 초기값이 없는 변수라면 .bss영역
+
+
+// signed : 부호존재 (+/-)
+int hp = 100;
+char a; // 1
+short b; // 2
+int c; // 4
+long long d; // 8
+
+// unsigned : 부호없음. 변수의 범위 2배(0~양수)
+// 양수로만 존재할 수 있는 데이터는 unsigned?
+// 무조건 이걸 사용할 건지는 의견이 갈림
+// data간의 자료형 고민 무조건 해야함 !!
+
+// 오버플로우와 언더플로우(범위보다 적을때) 조심
+
+int main()
+{
+	cout << "체력이 " << hp << " 남았습니다" << endl;
+}
---
+++ lecture/02_bool과 부동소수점.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+// boolean(불리언)과 실수
+
+// bool : 참/거짓
+bool isHighleve = true;
+bool isPlayer = true;
+bool isMale = false;
+
+// bool은 1바이트(8비트) 정수에 불과 (0/1)
+// 참과 거짓 둘 중 하나라는 힌트를 주기에 가독성이 좋다
+
+
+// 실수(부동소수점)
+// 1 2 3 4 int
+// 3.14 0.345  
+// float double(기본 세팅)
+
+// 부동소수점
+// .을 유동적으로 움직이며 표혀하는 방법
+
+// 3.1415926535
+// 1) 정규화 = 0.31415926535 * 10
+// 2) 31415926535 (유효숫자) 1(지수)
+
+// float 부호(1) 지수(8) 유효숫자(23) = 32비트 = 4바이트
+// double 부호(1) 지수(11) 유효숫자(52) = 64비트 = 8바이트
+
+float attackSpeed = 0.639f; // 4바이트
+double attackSpeend2 = 123.4123; // 8바이트
+
+
+// 프로그래밍 할 때 부동소수점은 근사값이라는 것을 기억
+
+int main()
+{
+	
+}
