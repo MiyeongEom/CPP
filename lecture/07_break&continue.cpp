44f1556215f82da7a649370268e28e4e30555424
Eom Mi Yeong <aldjd020420@naver.com>
2023-09-25 23:32:21 +0900

    09. 25.

---
+++ lecture/07_break&continue.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+// 반복문
+
+int main()
+{
+	int round = 1;
+	int hp = 100;
+	int damage = 27;
+
+	// 무한루프 : 전투시작
+	while (true)
+	{
+		hp -= damage;
+		if (hp < 0)
+			hp = 0;
+
+		// 시스템 메시지 출력
+		cout << "Round " << round << " 몬스터 체력 " << hp << endl;
+
+		if (hp == 0)
+		{
+			cout << "몬스터 처치!" << endl;
+			break;
+		}
+		
+		if (round == 5)
+		{
+			cout << "제한 라운드 종료" << endl;
+			break;
+		}
+		round++;
+	}
+
+
+	// continue 예제
+
+	for (int count = 1; count <= 10; count++)
+	{
+		bool isEven = ((count % 2) == 0);
+		if (isEven)
+			continue;
+
+		cout << count << endl;
+	}
+
+}
+
---
+++ lecture/08-함수.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+// 함수 (프로시저, 메소드, 루틴)
+
+/*
+input으로 무엇을 받고, output으로 무엇을 뱉을지 정해준다
+
+반환타입 함수이름 ({인자타입 매개변수})
+{
+	함수 내용
+	return ----;
+}
+*/
+
+void PrintfHelloWorld()
+{
+	cout << "hello world" << endl;
+
+	return;
+}
+
+void PrintNumber(int num)
+{
+	cout << "넘겨준 숫자는 " << num << "입니다" << endl;
+}
+
+int MultiplyBy2(int a)
+{
+	int result = a * 2;
+	return result;
+}
+
+// f5
+// f10
+// f11
+
+int main()
+{
+	int result = MultiplyBy2(3);
+	PrintNumber(result);
+}
