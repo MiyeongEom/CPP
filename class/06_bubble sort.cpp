269b13ca8f0d261ebf880b94bf4dffa3cd76300e
Eom Mi Yeong <aldjd020420@naver.com>
2023-10-24 08:04:39 +0900

    정렬 1

---
+++ class/06_bubble sort.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <format>
+#include <random>
+// 정렬(Sort) - bubble sort
+
+void change(int&, int&);
+
+int main()
+{
+	// [문제] 
+	// a를 오름차순으로 정렬하라.
+	// ascending order
+
+	int a[]{ 10, 8, 6, 4, 2, 9, 7, 5, 3, 1 };
+
+	for (int j = 9; j > 0 ; j--)
+		for (int i = 0; i < j; ++i)
+			if (a[i] > a[i + 1])
+				change(a[i], a[i + 1]);
+
+	for (int n : a)
+		std::cout << n << ' ';
+	std::cout << std::endl;
+}
+
+void change(int& p, int& q)
+{
+	int temp{ p };
+	p = q;
+	q = temp;
+}
