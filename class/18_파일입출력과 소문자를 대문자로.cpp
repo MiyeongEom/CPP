97f6196b15ccaf595f8fd43a92f23703375acec5
Eom Mi Yeong <aldjd020420@naver.com>
2023-10-26 10:05:16 +0900

    소문자를 대문자로

---
+++ class/18_파일입출력과 소문자를 대문자로.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <format>
+#include <random>
+#include <fstream>
+
+using namespace std;
+
+default_random_engine dre;
+uniform_int_distribution uid;
+
+int main()
+{
+    // [문제] 파일 "Cpp2.cpp"에 있는 알파벳 소문자를 모두 대문자로 바꿔
+    // "소스대문자.txt"에 기록하시오
+
+    ifstream in{ "Cpp2.cpp" };
+    ofstream out{ "소스대문자.txt" };
+
+    if (not in)
+        return 0;
+
+    int count[26]{ };
+
+    char filech;
+    in >> noskipws; //공백포함
+    while (in >> filech) {
+        if (islower(filech)) {
+            filech = toupper(filech);
+        }
+
+        out.put(filech);
+    }
+
+}
+
+// qqq
