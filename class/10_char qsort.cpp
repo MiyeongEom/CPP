92819527df9a76fc8010d38f618480d75acef698
Eom Mi Yeong <aldjd020420@naver.com>
2023-10-26 10:02:06 +0900

    문자열 오름차순

---
+++ class/10_char qsort.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <format>
+#include <random>
+// 정렬(Sort) - qsort / 제네릭 함수
+using namespace std;
+
+int main()
+{
+	// [문제] alpha를 오름 차순으로 정렬하라
+
+	char alpha[]{ "the quick brown fox jumps over the lazy log" };
+
+	int f(const void*, const void*);
+	qsort(alpha, strlen(alpha), sizeof(char), f);
+
+	cout << alpha << ' ';
+}
+
+int f(const void* a, const void* b)
+{
+	return *(char*)a - *(char*)b;
+}
