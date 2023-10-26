7efd7137ab3f061b15df3898734ce2297935fdc7
Eom Mi Yeong <aldjd020420@naver.com>
2023-10-26 10:02:20 +0900

    람다 이용한 정렬

---
+++ class/11_qsort lamda .cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <format>
+#include <random>
+// 정렬(Sort) - qsort
+using namespace std;
+
+int main()
+{
+	int a[]{ 2,4,6,8,10,9,7,5,3,1 };
+
+	//int f(const void*, const void*);
+
+	//qsort(a, 10, 4, f);   // f함수 본문을 알 수 없어 어떤 정렬? - 가독성 나쁨
+						   // f함수를 만들어야 했나?
+
+	// 이름 없는 함수 람다(lamda)
+	qsort(a, 10, 4, [](const void* a, const void* b) {
+		return *(int*)b - *(int*)a;
+		});
+
+	for (int i : a)
+		cout << i << ' ';
+	cout << '\n';
+}
+
+//int f(const void* a, const void* b)
+//{
+//	return *(int*)b - *(int*)a;
+//}
