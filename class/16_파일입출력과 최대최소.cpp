8f312ebeafe5ed15114bc212f3244979e985977b
Eom Mi Yeong <aldjd020420@naver.com>
2023-10-26 10:04:52 +0900

    파일입출력 최대최소

---
+++ class/16_파일입출력과 최대최소.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <format>
+#include <random>
+#include <fstream>
+
+using namespace std;
+
+default_random_engine dre;
+uniform_int_distribution uid;
+
+int main()
+{
+	/*
+	ofstream out{ "int만개.txt" };
+
+	for (int i = 0; i < 1'0000; ++i) {
+		out << uid(dre) << ' ';
+		if (((i + 1) % 10) == 0)
+			out << ' ';
+	}
+	*/
+
+	ifstream in{ "int만개.txt" };
+
+	if (not in) {
+		return 0;
+	}
+
+	int num;
+	int max = 0;
+	int min{ numeric_limits<int>::max() };
+
+	while (in >> num) {
+		// cout << num << ' ';
+		if (num > max)
+			max = num;
+
+		if (num < min)
+			min = num;
+	}
+
+	cout << max << endl;
+	cout << min << endl;
+}
