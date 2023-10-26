253939c956adf2c2fbe6338f98e53222f803a76f
Eom Mi Yeong <aldjd020420@naver.com>
2023-10-26 10:05:23 +0900

    소문자 개수

---
+++ class/19_문자 파일입출력과 소문자 개수.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <format>
+#include <random>
+#include <fstream>
+
+using namespace std;
+
+default_random_engine dre;
+uniform_int_distribution uid;
+
+int main()
+{
+    // [문제] 파일 "소스.cpp"에 있는 알파벳 소문자의 개수를 세서 다음과 같이 출력해라
+    // a - 3
+    // b - 1
+    // ....
+    // z - 0
+
+
+    ifstream in{ "Cpp2.cpp" };
+    if (not in)
+        return 0;
+
+    int count[26] = { 0 };
+
+    char filech;
+
+    while (in >> filech) {
+        if (islower(filech)) {
+            count[filech - 'a']++;
+        }
+    }
+
+    for (char ch = 'a'; ch <= 'z'; ++ch) {
+        std::cout << ch << " - " << count[ch - 'a'] << std::endl;
+    }
+}
