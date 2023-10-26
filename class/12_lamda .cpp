af93a31d592a345754da45b028e42e6035468637
Eom Mi Yeong <aldjd020420@naver.com>
2023-10-26 10:02:24 +0900

    람다

---
+++ class/12_lamda .cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <format>
+#include <random>
+// 정렬(Sort) - qsort
+using namespace std;
+
+int main()
+{
+	[]() {
+		cout << "안녕 난 람다야" << endl;
+	}();  // () - function call operator
+
+	cout << "메인" << endl;
+}
