81c50b7bda9a4bb5ffb889aa1311e36ca5cf6969
Eom Mi Yeong <aldjd020420@naver.com>
2023-10-26 10:05:05 +0900

    문자 개수 찾기

---
+++ class/18_문자 파일입출력과 특정 단어 개수 찾기.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <format>
+#include <random>
+#include <fstream>
+
+using namespace std;
+
+default_random_engine dre;
+uniform_int_distribution uid;
+
+int main()
+{
+	// [문제] 어떤 문자를 찾기 원하는지 물어봐라
+	// 파일 "소스.cpp"에 그 문자가 몇 개 있는지 알려주자
+	// 이 과정을 영원히 반복하라
+
+	ifstream in{ "Cpp2.cpp" };
+	if (not in)
+		return 0;
+
+	char a;
+	cout << "찾을 문자가 뭔가요 ? : ";
+	cin >> a;
+
+	char filech;
+	int count{};
+	while (in >> filech)
+		if (a == filech)
+			count++;
+
+	cout << count;
+}
