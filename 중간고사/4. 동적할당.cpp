9d13da25ed01df0dbd02c4de9ea222d3c10349a2
Eom Mi Yeong <aldjd020420@naver.com>
2023-10-30 23:43:18 +0900

    + 문제

---
+++ 중간고사/4. 동적할당.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void init(int*& p, int size) {
+	p = new int[size];
+	for (int i = 0; i < size; ++i) {
+		p[i] = 0;
+	}
+}
+
+int main()
+{
+	int* p{ nullptr };
+	init( p, 2022 );
+
+	for (int i = 0; i < 2022; ++i)
+		cout << p[i] << " ";
+
+	cout << endl;
+	delete[] p;
+}
---
+++ 중간고사/5. 문자파일 출력과 알파벳 별 확률 출력 .cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const int N{ 1'110'000 };
+char a[N];
+
+int main()
+{
+	ifstream in{ "문자들.txt" };
+
+    if (not in)
+        return 0;
+
+
+    for (int i = 0; i < N; ++i) {
+        in >> a[i];
+    }
+
+
+    int count[26] = { 0 };
+
+    for (int i = 0; i < N; ++i) {
+        if (islower(a[i])) {
+            count[a[i] - 'a']++;
+        }
+    }
+
+    for (char ch = 'a'; ch <= 'z'; ++ch) {
+        double probability = static_cast<double>(count[ch - 'a']) / N;
+        cout << ch << " - " << probability << endl;
+    }
+}
